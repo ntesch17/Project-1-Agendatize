399f0603833ec620615b6c8d661abec93593342f
Teschy <nlt5582@rit.edu>
2021-03-05 14:01:15 -0500

    Project 1... Getting there!

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -2,67 +2,127 @@
 // When node shuts down this will be cleared.
 // Same when your heroku app shuts down from inactivity
 // We will be working with databases in the next few weeks.
-const users = {};
-
-const respondJSON = (request, response, status, object) => {
-  response.writeHead(status, { 'Content-Type': 'application/json' });
-  response.write(JSON.stringify(object));
+const assignment = {};
+const title = {};
+const respond = (request, response, status, object, type) => {
+  response.writeHead(status, { 'Content-Type': type });
+  response.write(object);
   response.end();
 };
 
-const respondJSONMeta = (request, response, status) => {
-  response.writeHead(status, { 'Content-Type': 'application/json' });
+const respondMeta = (request, response, status, type) => {
+  response.writeHead(status, { 'Content-Type': type });
   response.end();
 };
 
-const getUsers = (request, response) => {
-  const responseJSON = {
-    users,
+const getUsers = (request, response, acceptedTypes) => {
+  let trueCount = true;
+  let count = 0;
+
+  if(trueCount){
+    count++;
+    trueCount = false;
+  }
+
+  const responsesMessage = {
+    message: 'Assignment Added!',
+    assignment: `${JSON.stringify(assignment)}`,
+
   };
-
-  respondJSON(request, response, 200, responseJSON);
-  return respondJSONMeta(request, response, 200);
+  
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message>${responsesMessage.message}</message>`;
+    responseXML = `${responseXML} <assignment>${responsesMessage.assignment}</assignment>`;
+    responseXML = `${responseXML} </response>`;
+    respond(request, response, 200, responseXML, 'text/xml');
+    return respondMeta(request, response, 200, 'text/xml');
+  }
+  const res = JSON.stringify(responsesMessage);
+  respond(request, response, 200, res, 'application/json');
+  return respondMeta(request, response, 200, 'application/json');
 };
 
-const addUser = (request, response, body) => {
-  const responseJSON = {
+const addUser = (request, response, body, acceptedTypes) => {
+  const responsesMessage = {
     message: 'Message: The Assignments title, description, and due date are all required',
     id: 'Bad Request',
   };
 
   if (!body.title || !body.description || !body.dueDate) {
-    responseJSON.id = 'ID: Missing Params';
-    return respondJSON(request, response, 400, responseJSON);
+    responsesMessage.id = 'ID: Missing Params';
+
+    if (acceptedTypes[0] === 'text/xml') {
+      let responseXML = '<response>';
+      responseXML = `${responseXML} <message>${responsesMessage.message}</message>`;
+      responseXML = `${responseXML} <id>${responsesMessage.id}</id>`;
+      responseXML = `${responseXML} </response>`;
+  
+      respond(request, response, 400, responseXML, 'text/xml');
+      return respondMeta(request, response, 400, 'text/xml');
+    }
+
+    const res = JSON.stringify(responsesMessage);
+    respond(request, response, 400, res, 'application/json');
+    return respondMeta(request, response, 400, 'application/json');
   }
 
   let responseCode = 201;
 
-  if (users[body.title]) {
+  if (assignment[body.title]) {
     responseCode = 204;
   } else {
-    users[body.title] = {};
-    users[body.title].title = body.title;
+    assignment[body.title] = {};
+    assignment[body.title].title = body.title;
   }
 
-  users[body.title].dueDate = body.dueDate;
+  assignment[body.title].dueDate = body.dueDate;
+  assignment[body.title].description = body.description;
 
   if (responseCode === 201) {
-    responseJSON.message = 'Message: Created Successfuly!';
-    responseJSON.id = 'Success';
-    return respondJSON(request, response, responseCode, responseJSON);
+    responsesMessage.message = 'Message: Created Successfuly!';
+    responsesMessage.id = 'Success';
+
+    if (acceptedTypes[0] === 'text/xml') {
+      let responseXML = '<response>';
+      responseXML = `${responseXML} <message>${responsesMessage.message}</message>`;
+      responseXML = `${responseXML} <id>${responsesMessage.id}</id>`;
+      responseXML = `${responseXML} </response>`;
+  
+      respond(request, response, responseCode, responseXML, 'text/xml');
+      return respondMeta(request, response, responseCode, 'text/xml');
+    }
+
+    const res = JSON.stringify(responsesMessage);
+    respond(request, response, responseCode, res, 'application/json');
+    return respondMeta(request, response, responseCode, 'application/json');
   }
 
-  return respondJSONMeta(request, response, responseCode);
+  // if (acceptedTypes[0] === 'text/xml') {
+  //   return respondMeta(request, response, responseCode, 'text/xml');
+  // }
+  //return respondMeta(request, response, responseCode, 'application/json');
 };
 
-const notFound = (request, response) => {
+const notFound = (request, response, acceptedTypes) => {
   const responsesMessage = {
     message: 'Message: The page you are looking for was not found.',
-    id: 'ID: Not Found',
+    id: 'ID: notFound',
   };
 
-  respondJSON(request, response, 404, responsesMessage);
-  return respondJSONMeta(request, response, 404);
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message>${responsesMessage.message}</message>`;
+    responseXML = `${responseXML} <id>${responsesMessage.id}</id>`;
+    responseXML = `${responseXML} </response>`;
+
+    respond(request, response, 404, responseXML, 'text/xml');
+    return respondMeta(request, response, 404, 'text/xml');
+  }
+
+  const res = JSON.stringify(responsesMessage);
+  respond(request, response, 404, res, 'application/json');
+  return respondMeta(request, response, 404, 'application/json');
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const handlePost = (request, response, parsedUrl) => {
+const handlePost = (request, response, parsedUrl,acceptedTypes) => {
   if (parsedUrl.pathname === '/addUser') {
     const body = [];
 
@@ -24,30 +24,30 @@ const handlePost = (request, response, parsedUrl) => {
       const bodyString = Buffer.concat(body).toString();
       const bodyParams = query.parse(bodyString);
 
-      jsonHandler.addUser(request, response, bodyParams);
+      jsonHandler.addUser(request, response, bodyParams, acceptedTypes);
     });
   }
 };
 
-const handleGet = (request, response, parsedUrl) => {
+const handleGet = (request, response, parsedUrl, acceptedTypes) => {
   if (parsedUrl.pathname === '/style.css') {
     htmlHandler.getCSS(request, response);
   } else if (parsedUrl.pathname === '/getUsers') {
-    jsonHandler.getUsers(request, response);
+    jsonHandler.getUsers(request, response,acceptedTypes);
   } else if (parsedUrl.pathname === '/') {
     htmlHandler.getIndex(request, response);
   } else {
-    jsonHandler.notFound(request, response);
+    jsonHandler.notFound(request, response,acceptedTypes);
   }
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
-
+  const acceptedTypes = request.headers.accept.split(',');
   if (request.method === 'POST') {
-    handlePost(request, response, parsedUrl);
+    handlePost(request, response, parsedUrl, acceptedTypes);
   } else {
-    handleGet(request, response, parsedUrl);
+    handleGet(request, response, parsedUrl, acceptedTypes);
   }
 };
 
